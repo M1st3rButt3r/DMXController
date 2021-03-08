9c0c07ffa60ce6dd61f1ef1957680db6e6cfef75
Bennet Rupnow <bennet.rupnow@gym-meiendorf.de>
2021-03-08 20:13:50 +0100

    Added Loading of main.fxml

--- src/com/dmxcontroller/main/Main.java
+++ src/com/dmxcontroller/main/Main.java
@@ -1,8 +1,12 @@
 package com.dmxcontroller.main;
+
 import com.dmxcontroller.data.Channel;
 import com.dmxcontroller.data.Interface;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -12,13 +16,18 @@ public class Main extends Application{
 	public static void main(String[] args) {
 		//Create DMX Interface
 		dmxInterface = new Interface("");
-		dmxInterface.Send(new Channel(0, 255, 0));
+		dmxInterface.send(new Channel(0, 255, 0));
+		launch(args);
 	}
 
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
+	public void start(Stage stage) throws Exception {
+		Parent parent = FXMLLoader.load(getClass().getResource("../scenes/main.fxml"));
 		
+		stage.setScene(new Scene(parent, 970, 540));
+		stage.setTitle("DMX Controller");
+		stage.show();
 	}
-
+	
+	
 }
