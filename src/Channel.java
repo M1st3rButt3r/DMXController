5a4353307602d0cd5cfbc8fcf861f7aff073bcda
Bennet Rupnow <bennet.rupnow@gym-meiendorf.de>
2021-03-08 15:44:24 +0100

    Added JavaFX

--- src/Channel.java
+++ src/Channel.java
@@ -1,11 +1,13 @@
 
 public class Channel {
+	public int universe;
 	public int channel;
 	public int value;
 	
-	public Channel(int channel, int value) {
+	public Channel(int channel, int value, int universe) {
 		//TODO Clamp values
 		this.channel = channel > 512 ? 512 : channel < 1 ? 1 : channel;
 		this.value =  value > 255 ? 255 : value < 0 ? 0 : value;
+		this.universe = universe;
 	}
 }
